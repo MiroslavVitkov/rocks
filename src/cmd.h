5c080b11ee8b7da213551acbc88cf6a6c8780ed0
Miroslav Vitkov <sir.vorac@gmail.com>
2019-05-25 16:41:52 +0200

    [io] Correctly reading in the dataset. Also most of the commented out code from the previous project was deleted.

--- src/cmd.h
+++ src/cmd.h
@@ -24,36 +24,13 @@ struct NoOp : Base
 };
 
 
-// https://physics.nist.gov/cgi-bin/ASD/lines1.pl?compositionCu&spectra=Cu&low_w=180&limits_type=0&upp_w=961&show_av=2&unit=1&resolution=1&temp=1&eden=1e17&libs=1
-// abscissa - from 180.0nm to 960.9nm inclusive with 0.1nm step.
-// ordinate - radiance, W·sr−1·m−2
-// number of points - 7810
-struct Spectrum
-{
-
-};
-
-struct Corpus
-{
-    std::vector<Spectrum> _spectra;
-    const std::string _label;
-};
-
-// Example of the file structure that we are expecting.
-//data
-//├── azurite
-//│   ├── spot00
-//│   ├── spot01
-//│   ├── spot10
-//│   ├── spot11
-//├── brochantite
-//│   ├── spot0
-//│   ├── spot1
-//│   ├── spot2
 struct Train : Base
 {
-    Train( const std::string & data_dir);
-    void execute() override {}
+    Train( const std::string & data_dir = "./data/" );
+    void execute() override;
+
+private:
+    const std::string _data_dir;
 };
 
 
--- src/io.cpp
+++ src/io.cpp
@@ -5,11 +5,11 @@
 #include <fstream>
 
 
-namespace fs = std::filesystem;
-
-
 namespace io
 {
+
+
+namespace fs = std::filesystem;
 
 
 // Waiting for C++20's std::basic_string::ends_with().
@@ -30,19 +30,34 @@ Spectrum read_csv( const fs::path & path )
 
     // Drop the column names.
     std::getline( file, line);
-    assert( line == "wavelength,intensity" );
+    assert( line == "wavelength,intensity\r" );
 
     Spectrum ret;
     auto it = ret._y.begin();
 
-    while( file )
+    double wavelength, intensity;
+    char comma;
+    while( file >> wavelength >> comma >> intensity )
     {
-        double wavelength, intensity;
-        char comma;
-        file >> wavelength >> comma >> intensity;
         *it++ = intensity;
     }
+    // Make sure we read the correct number of datapoints.
     assert( it == ret._y.end() );
+
+    return ret;
+}
+
+
+std::vector< std::string > get_subdirs( const std::string & path )
+{
+    std::vector< std::string > ret;
+    for( const auto & dir : fs::directory_iterator( path ) )
+    {
+        if( fs::is_directory( dir ) )
+        {
+            ret.push_back( dir.path().filename() );
+        }
+    }
 
     return ret;
 }
@@ -51,20 +66,14 @@ Spectrum read_csv( const fs::path & path )
 Dataset read_dataset( const std::string & path )
 {
     // All top-level dirs found in 'path' are label names.
-    std::vector<std::string> labels;
-    for( const auto & dir : fs::directory_iterator( path ) )
-    {
-        if( fs::is_directory( dir ) )
-        {
-            labels.push_back( dir.path().filename() );
-        }
-    }
+    const auto labels{ get_subdirs( path ) };
 
     // All .csv files under a label are samples of that label.
     Dataset ret;
     for( const auto & l : labels )
     {
-        for( const auto & file : fs::recursive_directory_iterator( path + l ) )
+        for( const auto & file
+           : fs::recursive_directory_iterator( path + '/' + l ) )
         {
             if( ends_with( file.path().filename(), ".csv" ) )
             {
@@ -75,75 +84,6 @@ Dataset read_dataset( const std::string & path )
 
     return ret;
 }
-
-/*
-
-std::vector<std::tuple<std::string, std::vector<Spectrum>>>
-read_dataset( const std::string & path)
-{
-
-}
-
-
-
-Spectrum read_file( const std::string & path );
-void read_dir( std::vector<Spectrum> & spectra
-             , std::string & label
-             , const std::string path );
-std::vector<std::string> get_subdirs( const std::string & path );
-
-Spectrum read_file( const std::string & path );
-
-
-void read_dir( std::vector<Spectrum> & spectra
-             , std::string & label
-             , const std::string path );
-
-
-// True if entry is the unix current of parent directory.
-bool is_auto_dir( const struct dirent * const ent )
-{
-    bool is_currdir = ! std::string( ent->d_name ).compare( "." );
-    bool is_pardir  = ! std::string( ent->d_name ).compare( ".." );
-    return is_currdir || is_pardir;
-}
-
-
-std::vector<std::string> get_subdirs( const std::string & path )
-{
-    DIR * dir = opendir( path.c_str() );
-    if( ! dir )
-    {
-        throw Exception{ "Failed to open dir: " + path };
-    }
-
-    std::vector<std::string> out;
-    while( struct dirent * stream = readdir( dir ) )
-    {
-        assert( stream );
-
-        if( is_auto_dir( stream ) )
-        {
-            continue;
-        }
-
-        std::string temp{ &stream->d_name };
-        out.insert( temp );
-    }
-    closedir( dir );
-
-    return out;
-}
-
-
-
-
-
-
-
-std::vector<std::tuple<std::string, std::vector<Spectrum>>>
- read_dataset( const std::string & path);
-*/
 
 
 }  // namespace io
