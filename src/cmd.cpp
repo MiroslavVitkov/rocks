22e01e30356928b42d1d1e7e1f1cc7fa4e7dc534
Miroslav Vitkov <sir.vorac@gmail.com>
2019-06-27 18:58:51 +0200

    [build] Qwt successfully integrated.

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -5,8 +5,12 @@
 #include "score.h"
 
 // TODO: remove those includes, they are here for testing purposes.
-#include <QtCore>
+#include <QApplication>
 #include <qwt_plot.h>
+#include <qwt_plot_curve.h>
+#include <qwt_plot_grid.h>
+#include <qwt_symbol.h>
+#include <qwt_legend.h>
 
 #include <iostream>
 #include <numeric>
@@ -59,6 +63,43 @@ ReportOutliers::ReportOutliers( const std::string & data_dir )
 
 void ReportOutliers::execute()
 {
+    int argc = 7;
+    char ** argv{};
+    QApplication a( argc, argv );
+
+    QwtPlot plot;
+    plot.setTitle( "Plot Demo" );
+    plot.setCanvasBackground( Qt::white );
+    plot.setAxisScale( QwtPlot::yLeft, 0.0, 10.0);
+    plot.insertLegend( new QwtLegend() );
+
+    QwtPlotGrid *grid = new QwtPlotGrid();
+    grid->attach( &plot );
+
+    QwtPlotCurve *curve = new QwtPlotCurve();
+    curve->setTitle( "Pixel Count" );
+    curve->setPen( Qt::blue, 4 ),
+    curve->setRenderHint( QwtPlotItem::RenderAntialiased, true );
+
+    QwtSymbol *symbol = new QwtSymbol( QwtSymbol::Ellipse,
+        QBrush( Qt::yellow ), QPen( Qt::red, 2 ), QSize( 8, 8 ) );
+    curve->setSymbol( symbol );
+
+    QPolygonF points;
+    points << QPointF( 0.0, 4.4 ) << QPointF( 1.0, 3.0 )
+        << QPointF( 2.0, 4.5 ) << QPointF( 3.0, 6.8 )
+        << QPointF( 4.0, 7.9 ) << QPointF( 5.0, 7.1 );
+    curve->setSamples( points );
+
+    curve->attach( &plot );
+
+    plot.resize( 600, 400 );
+    plot.show();
+
+    a.exec();
+
+    ////////////////////////////////
+
     const auto spectra = io::read( _data_dir );
 
     // Measure.
